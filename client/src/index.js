a4007ae34f7621a2a57ba3c219d7f6e069c1bd41
Aly Khaled <alykhaled2001@live.com>
2021-07-27 16:22:06 +0200

    Add React side and routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql`
+    query getPosts{
+      getPosts {
+        id
+        body
+        username
+        createdAt
+      }
+    }
+  `
+}).then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +33,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -5,32 +5,32 @@ const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
-        async getPosts(){
-          try{
-            const posts = await Post.find().sort({createdAt: -1});
-            return posts;
+      async getPosts(){
+        try{
+          const posts = await Post.find().sort({createdAt: -1});
+          return posts;
+        }
+        catch(error){
+          throw new Error(error);
+        }
+      },
+      async getPost(_,{postId})
+      {
+        try{
+          const post = await Post.findById(postId);
+          if(post)
+          {
+            return post;
           }
-          catch(error){
-            throw new Error(error);
-          }
-        },
-        async getPost(_,{postId})
-        {
-          try{
-            const post = await Post.findById(postId);
-            if(post)
-            {
-              return post;
-            }
-            else
-            {
-              throw new Error('Post not found');
-            }
-          }
-          catch(error){
-            throw new Error(error);
+          else
+          {
+            throw new Error('Post not found');
           }
         }
+        catch(error){
+          throw new Error(error);
+        }
+      }
     },
     Mutation: {
       async createPost(_,{ body },context)
@@ -45,6 +45,11 @@ module.exports = {
           createdAt: new Date().toISOString(),
         })
         const post = await newPost.save();
+
+        context.pubsub.publish('NEW_POST', {
+          newPost: post
+        });
+
         return post;
       
       },
@@ -96,6 +101,11 @@ module.exports = {
         catch(error){
           throw new Error(error);
         }
+      }
+    },
+    Subscription: {
+      newPost: {
+        subscribe : (_,__,{ pubsub }) => pubsub.asyncIterator('NEW_POST')
       }
     }
 }
